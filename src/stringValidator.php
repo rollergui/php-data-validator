64e9350e4ba06ca68d6306a8226973fa5f5718fe
gui <guilhermelionribeiro@gmail.com>
2018-12-10 00:45:03 -0200

    include length option validation

--- src/stringValidator.php
+++ src/stringValidator.php
@@ -1,7 +1,43 @@
 <?php
 
+const STRING_OPTIONS = [
+    'length' => 'checkLength'
+];
+
 function validateString($value, $options = []) {
-    return is_string($value);
+    if (!options) return is_string($value);
+    return (is_string($value) && checkStringOptions($options, $value));
+}
+
+function checkStringOptions($options, $value) {
+    foreach($options as $option) {
+        $opt = strstr($option, '(', true);
+        $args = strstr($option, '(');
+        if (in_array($opt, array_keys(STRING_OPTIONS))) {
+            if (call_user_func(STRING_OPTIONS[$opt], $args, $value)) continue;
+            if ($validatedOptions) continue; else return false;
+        } else {
+            return 'Option unknown.';
+        }
+    }
+    return true;
+}
+
+function checkLength($args, $value) {
+    $val = strlen($value);
+    $arg = explode(' ', str_replace(['(', ')'], '', $args));
+    switch($arg[0]) {
+        case '=':
+            return $val == $arg[1];
+        case '<':
+            return $val < $arg[1];
+        case '>':
+            return $val > $arg[1];
+        case '<=':
+            return $val <= $arg[1];
+        case '>=':
+            return $val >= $arg[1];
+    }
 }
 
 ?>
